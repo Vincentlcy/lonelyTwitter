a4651807e15850b5685d445060835c9e34505189
Vincentlcy <liuchuyang1997@outlook.com>
2018-02-27 18:42:39 -0700

    TEMP CHANGED ADDED

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
@@ -2,10 +2,15 @@ package ca.ualberta.cs.lonelytwitter;
 
 import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+
+import com.robotium.solo.Solo;
 
 import junit.framework.TestCase;
 
 public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 {
+
+    private Solo solo;
 
     public LonelyTwitterActivityTest() {
         super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
@@ -13,6 +18,26 @@ public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 
 
     public void testStart() throws Exception {
         Activity activity = getActivity();
+    }
 
+    public void setUp() throws Exception {
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    public void testAddingTweetToTheList() {
+        solo.assertCurrentActivity("Worry Activity", LonelyTwitterActivity.class);
+        solo.enterText((EditText) solo.getView(R.id.body), "Text Tweet!");
+        solo.clickOnButton("Save");
+        solo.enterText((EditText) solo.getView(R.id.body), "");
+
+        assertTrue(solo.searchText("Test Tweet!"));
+        solo.clickOnButton("Clear");
+        assertFalse(solo.searchText("Text Tweet!"));
+
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
     }
 }
